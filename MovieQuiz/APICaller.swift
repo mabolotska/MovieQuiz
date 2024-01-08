5972b5dc03ce81174c6304b6d8e8df8d4be20615
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2024-01-08 19:38:27 +0100

    set UI

---
+++ MovieQuiz/APICaller.swift
@@ -0,0 +1,8 @@
+//
+//  APICaller.swift
+//  MovieQuiz
+//
+//  Created by Maryna Bolotska on 08/01/24.
+//
+
+import Foundation
---
+++ MovieQuiz/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  MovieQuiz
+//
+//  Created by Maryna Bolotska on 08/01/24.
+//
+
+import Foundation
